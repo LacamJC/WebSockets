6b2f4f52dabf9e899147855e86cb7615b917d84f
LacamJC <joaoramajo744@gmail.com>
2024-02-16 19:29:48 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io')
+
+const port = 3000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req,res)=>{
+    res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) =>{
+    console.log('Um usuario se conectou');
+
+    socket.on('chat message', (msg)=>{
+        io.emit('Hello', msg);
+    });
+
+
+    socket.on('chat message', (msg)=>{
+        console.log(`Message : ${msg}`)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+});
+
+
+
+server.listen(port, ()=>{
+    console.log(`Server runnint at port ${port}`);
+})
